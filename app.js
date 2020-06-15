218c652e26a9e9a8ff0a6ac9c549bb825646bf7c
spythonian <spythonian@gmail.com>
2020-06-15 16:30:33 +0100

    Initialized landing page and set ejs view engine

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+// Initialized express server
 const express = require('express');
 const app = express();
+// Port
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello TourCamp App'));
+// set view engine
+app.set('view engine', 'ejs')
 
+// Route
+app.get('/', (req, res) => res.render('landing'));
+
+app.get('/campgrounds', (req, res) => ())
+
+// Listened port
 app.listen(port, () => console.log(`TourCamp Server is listening at http://localhost:${port}`))
