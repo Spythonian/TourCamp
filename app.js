e0b42e486810cc9e78940bf52fd17656318676f5
spythonian <spythonian@gmail.com>
2020-06-15 23:38:49 +0100

    Set css dependecies

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const port = 3000;
 // set view engine
 app.set("view engine", "ejs");
 
+// Prepared dependencies
+// app.use('/api', api); // redirect API calls
+app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+
 // Route
 app.get("/", (req, res) => res.render("landing"));
 
