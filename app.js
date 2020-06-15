158fd5d43d5fdc62571f9daec3bd1f328326215a
spythonian <spythonian@gmail.com>
2020-06-15 23:12:46 +0100

    Worked with dummy camp names and images

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 // Initialized express server
-const express = require('express');
+const express = require("express");
 const app = express();
 // Port
 const port = 3000;
 
 // set view engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // Route
-app.get('/', (req, res) => res.render('landing'));
+app.get("/", (req, res) => res.render("landing"));
 
-app.get('/campgrounds', (req, res) => {
-    const campgrounds = [
-        {name: 'Idumota Tour Camp', image: ''},
-        {name: 'Osogbo Tour Ground', image: ''},
-        {name: 'Ita agbon Camp', imgae: ''},
-    ]
-    res.render('campgrounds', {campgrounds:campgrounds});
+app.get("/campgrounds", (req, res) => {
+  const campgrounds = [
+    { name: "Idumota Tour Camp", image: "" },
+    { name: "Osogbo Tour Ground", image: "" },
+    { name: "Ita agbon Camp", imgae: "" },
+  ];
+  res.render("campgrounds", { campgrounds: campgrounds });
 });
 
 // Listened port
-app.listen(port, () => console.log(`TourCamp Server is listening at http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`TourCamp Server is listening at http://localhost:${port}`)
+);
