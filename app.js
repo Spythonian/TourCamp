533920b278223fc1a36ef1314acc837df5b7cabb
spythonian <spythonian@gmail.com>
2020-06-15 16:49:28 +0100

    Initialized and displayed campgrounds

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ app.set('view engine', 'ejs')
 // Route
 app.get('/', (req, res) => res.render('landing'));
 
-app.get('/campgrounds', (req, res) => ())
+app.get('/campgrounds', (req, res) => {
+    const campgrounds = [
+        {name: 'Idumota Tour Camp', image: ''},
+        {name: 'Osogbo Tour Ground', image: ''},
+        {name: 'Ita agbon Camp', imgae: ''},
+    ]
+    res.render('campgrounds', {campgrounds:campgrounds});
+});
 
 // Listened port
 app.listen(port, () => console.log(`TourCamp Server is listening at http://localhost:${port}`))
