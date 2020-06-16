d487b6c040cd2ea776c1097a76bb08d8cba46e34
spythonian <spythonian@gmail.com>
2020-06-16 08:59:31 +0100

    Installed and use body-parser

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // Initialized express server
-const express = require("express");
+const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
 // Port
 const port = 3000;
+
 
 // set view engine
 app.set("view engine", "ejs");
 
+// body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Prepared dependencies
-// app.use('/api', api); // redirect API calls
 app.use('/', express.static(__dirname + '/www')); // redirect root
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
@@ -19,13 +24,31 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 // Route
 app.get("/", (req, res) => res.render("landing"));
 
-app.get("/campgrounds", (req, res) => {
-  const campgrounds = [
+let campgrounds = [
     { name: "Idumota Tour Camp", image: "" },
     { name: "Osogbo Tour Ground", image: "" },
     { name: "Ita agbon Camp", imgae: "" },
   ];
+
+
+app.get("/campgrounds", (req, res) => {
+
   res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.post('/campgrounds', (req, res) => {
+    // res.send('You hit the post route')
+    // get data from form and add to campgrounds array
+    const name = req.body.name;
+    const image = req.body.image;
+    let newCampground = { name: name, image:image}
+    campgrounds.push(newCampground);
+    //redirect back to campgrounds page
+    res.redirect('/campgrounds');
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('new')
 });
 
 // Listened port
